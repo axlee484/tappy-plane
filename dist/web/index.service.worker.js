89f4af1a30c0b8e8880df96080fa04919e54878d
ayush <axlee484@users.noreply.github.com>
2025-12-26 01:55:47 +0530

    update build

--- dist/web/index.service.worker.js
+++ dist/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1766693762|4999237080';
+const CACHE_VERSION = '1766694340|5576514829';
 /** @type {string} */
 const CACHE_PREFIX = 'tappy-plane-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
